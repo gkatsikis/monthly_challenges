4e07b67074f5815b62b9988efa8f82b13c511450
gkatsikis <geo.bjj.kat@gmail.com>
2022-09-11 10:01:54 -0400

    moved key

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -19,10 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6bz-!uxq=ld!z9ak7wggrww&(hq1smsg@2*x3tk2%m%*ar0^m!"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
